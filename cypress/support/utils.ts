8d9795e2741c7e6b8c6acda4640d447d0ccea652
Pantilie Daniel <>
2024-06-30 19:02:28 +0300

    Add text file

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -1,16 +1,15 @@
 import { NonStaging, StagingEnv } from "../pageObjects/home";
 
-// cypress/support/utils.js (or utils.ts)
 export function closeAllModals() {
   // Define a selector for the close button, commonly found in modals
   const closeButtonSelectors = [
-    '.GeofencingShopSelector__content .close-button-selector', // example selector for GeofencingShopSelector
-    '.modal .close', // example selector for generic modals
-    '.popup .close', // example selector for generic popups
-    '.pop-in__close-button', // example selector for generic popups
-    '.GeofencingShopSelector__content', // example selector for generic popups
-    '.CloseIcon', // example selector for generic popups
-    '#shopify-pc__banner__btn-accept', // example selector for generic popups
+    '.GeofencingShopSelector__content .close-button-selector', 
+    '.modal .close', 
+    '.popup .close', 
+    '.pop-in__close-button', 
+    '.GeofencingShopSelector__content', 
+    '.CloseIcon',
+    '#shopify-pc__banner__btn-accept', 
     '.recommendation-modal__button',
     '.recommendation-modal__close-button-container',
     '.recommendation-modal__button',
@@ -21,9 +20,6 @@ export function closeAllModals() {
     '#button[aria-label="Close Modal"]',
     '.GeofencingShopSelector__flag--EU > .GeofencingShopSelector__flag-icon',
     '#onetrust-accept-btn-handler',
-
-    // example selector for generic popups
-    // Add more selectors as needed
   ];
   // Iterate over each selector and attempt to close the modal
   closeButtonSelectors.forEach((selector) => {
